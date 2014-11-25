e482f9f869627b4f92610dbdf1a449ee9819939f
YuyaOka <kigyouka@MacBook-Pro.local>
2014-11-26 01:28:44 +0900

    バグ潰し
    https://github.com/YuyaOka/Prog3_Shooting/issues/1

--- Shooting Game/Assets/Done/Create_JavaScript/Enemy_Ship_2.js
+++ Shooting Game/Assets/Done/Create_JavaScript/Enemy_Ship_2.js
@@ -4,8 +4,8 @@ var Enemy : Transform ;
 
 function Update ()
 {
-  if(Time.frameCount % 223 == 0)
+  if(Time.frameCount % 120 == 0)
   {
-    Instantiate(Enemy , Vector3(Random.Range(23.0 , -23.0) , 20 ,13) , transform.rotation) ;
+    Instantiate(Enemy , Vector3(Random.Range(19.5 , -19.5) , 0 ,13) , transform.rotation) ;
   }
 }
